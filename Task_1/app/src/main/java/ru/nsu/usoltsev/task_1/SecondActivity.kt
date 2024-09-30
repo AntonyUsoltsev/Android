49800e28fa459b0964b07ddd61ba9c0ee4f98666
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2024-09-30 22:03:31 +0700

    Add simple activities

--- Task_1/app/src/main/java/ru/nsu/usoltsev/task_1/SecondActivity.kt
+++ Task_1/app/src/main/java/ru/nsu/usoltsev/task_1/SecondActivity.kt
@@ -8,21 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_b)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_b)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val buttonVal = findViewById<Button>(R.id.button_a)
+        val buttonVal = findViewById<Button>(R.id.button_b)
         buttonVal.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
+            val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
